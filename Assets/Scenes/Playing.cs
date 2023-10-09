f2b1067ad9d0f6a34b4d90a52210f18b65bbffd8
Jordan Lowa <102396119+24033404@users.noreply.github.com>
2023-10-10 01:40:47 +1000

    Add files via upload

---
+++ Assets/Scenes/Playing.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playing : MonoBehaviour
+{
+    public AudioSource Bark;
+
+
+public void PlayBark() {
+Bark.Play ();
+
+}
+
+}
+
